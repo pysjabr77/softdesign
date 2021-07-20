fa4f927775fefce70461314603b792f6e586c63f
Pedro Yoda Saito <pedro.saito@foton.la>
2021-07-20 18:36:27 -0300

    Criando classe main do springboot

---
+++ src/main/java/br/com/pedroyodasaito/softdesign/SoftdesignApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedroyodasaito.softdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftdesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftdesignApplication.class, args);
+	}
+
+}
