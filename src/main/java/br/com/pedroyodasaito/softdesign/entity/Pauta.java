1013a7140d12560cd80baf6c0782fc9ee7a0f33f
Pedro Yoda Saito <pedro.saito@foton.la>
2021-07-20 19:46:50 -0300

    Criando entidade pauta

---
+++ src/main/java/br/com/pedroyodasaito/softdesign/entity/Pauta.java
@@ -0,0 +1,44 @@
+package br.com.pedroyodasaito.softdesign.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Objects;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "pauta")
+public class Pauta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column
+    private String nome;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pauta pauta = (Pauta) o;
+        return id.equals(pauta.id) && nome.equals(pauta.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+}
