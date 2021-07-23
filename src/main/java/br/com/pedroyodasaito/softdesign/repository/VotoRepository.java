ce6f264e01b253a8152a22f1b4ae94484ed314bb
Pedro Yoda Saito <pedro.saito@foton.la>
2021-07-22 21:17:54 -0300

    Refactor no service de voto e criando classe de NegocioException

--- src/main/java/br/com/pedroyodasaito/softdesign/repository/VotoRepository.java
+++ src/main/java/br/com/pedroyodasaito/softdesign/repository/VotoRepository.java
@@ -1,6 +1,7 @@
 package br.com.pedroyodasaito.softdesign.repository;
 
+import br.com.pedroyodasaito.softdesign.entity.Voto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VotoRepository extends JpaRepository {
+public interface VotoRepository extends JpaRepository<Voto, Integer> {
 }
--- src/main/java/br/com/pedroyodasaito/softdesign/service/impl/VotoServiceImpl.java
+++ src/main/java/br/com/pedroyodasaito/softdesign/service/impl/VotoServiceImpl.java
@@ -4,11 +4,14 @@ import br.com.pedroyodasaito.softdesign.api.dto.voto.VotarDTO;
 import br.com.pedroyodasaito.softdesign.entity.Associado;
 import br.com.pedroyodasaito.softdesign.entity.Sessao;
 import br.com.pedroyodasaito.softdesign.entity.Voto;
+import br.com.pedroyodasaito.softdesign.exception.NegocioException;
 import br.com.pedroyodasaito.softdesign.repository.AssociadoRepository;
 import br.com.pedroyodasaito.softdesign.repository.SessaoRepository;
 import br.com.pedroyodasaito.softdesign.repository.VotoRepository;
 import br.com.pedroyodasaito.softdesign.service.VotoService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class VotoServiceImpl implements VotoService {
@@ -30,11 +33,24 @@ public class VotoServiceImpl implements VotoService {
     public void votar(VotarDTO dto) {
         Voto voto = new Voto();
         voto.setVoto(dto.isVoto());
-        Sessao sessao = sessaoRepository.findById(dto.getSessaoId()).get();
-        voto.setSessao(sessao);
-        Associado associado = associadoRepository.findById(dto.getAssociadoId()).get();
-        voto.setAssociado(associado);
-
+        voto.setSessao(validarSessao(dto.getSessaoId()));
+        voto.setAssociado(validarAssociado(dto.getAssociadoId()));
         repository.save(voto);
+    }
+
+    private Sessao validarSessao(Integer sessaoId) {
+        Optional<Sessao> optionalSessao = sessaoRepository.findById(sessaoId);
+        if (!optionalSessao.isPresent()) {
+            throw new NegocioException("Sessão não existe");
+        }
+        return optionalSessao.get();
+    }
+
+    private Associado validarAssociado(Integer associadoId) {
+        Optional<Associado> optionalAssociado = associadoRepository.findById(associadoId);
+        if (!optionalAssociado.isPresent()) {
+            throw new NegocioException("Associado não existe");
+        }
+        return optionalAssociado.get();
     }
 }
